d3514ee4c6b75ef40137da362f41862dbbc6764b
Chisty Md.Muzammel Hossain <chistyz10@gmail.com>
2021-03-08 17:19:40 +0600

    Collab Commit

---
+++ Collaborator.java
@@ -0,0 +1,10 @@
+public class Collaborator{
+    string colabId;
+    string colabName;
+
+    public Collaborator(string colabId,string colabName)
+    {
+        this.colabId = colabId;
+        this.colabName = colabName;
+    }
+}
